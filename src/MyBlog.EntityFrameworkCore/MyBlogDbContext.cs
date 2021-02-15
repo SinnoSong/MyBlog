fc7983dd6fdb6faebb18407a1cad2eed6aaff058
锦辉 宋 <sinno.song@outlook.com>
2021-02-15 19:16:03 +0800

    添加实体模型

--- src/MyBlog.EntityFrameworkCore/MyBlogDbContext.cs
+++ src/MyBlog.EntityFrameworkCore/MyBlogDbContext.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Domain.Blog;
 using Volo.Abp.Data;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace MyBlog.EntityFrameworkCore
 {
+    [ConnectionStringName("Default")]
     public class MyBlogDbContext : AbpDbContext<MyBlogDbContext>
     {
-        [ConnectionStringName("Default")]
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<FriendLink> FriendLinks { get; set; }
+
         public MyBlogDbContext(DbContextOptions<MyBlogDbContext> options) : base(options)
         {
         }
