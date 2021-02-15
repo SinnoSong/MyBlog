2e5c1dff6d0462ab71542a9d5202247883a2edaf
锦辉 宋 <sinno.song@outlook.com>
2021-02-15 19:32:43 +0800

    Configure扩展方法中配置表模型

---
+++ src/MyBlog.Domain.Shared/MyBlogDbConsts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Domain.Shared
+{
+    public class MyBlogDbConsts
+    {
+        public static class DbTableName
+        {
+            public const string Posts = "Posts";
+            public const string Categories = "Categories";
+            public const string Tags = "Tags";
+            public const string PostTags = "PostTags";
+            public const string FriendLinks = "FriendLinks";
+        }
+    }
+}
