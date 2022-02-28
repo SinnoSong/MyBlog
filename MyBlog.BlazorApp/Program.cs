4059c5c8d5d679a2179046ede2e0119e049a0c93
Sinno.song <sinno.song@outlook.com>
2022-02-28 21:30:04 +0800

    修改样式，修改运行时

--- MyBlog.BlazorApp/Program.cs
+++ MyBlog.BlazorApp/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MyBlog.BlazorApp
@@ -15,7 +11,7 @@ namespace MyBlog.BlazorApp
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
